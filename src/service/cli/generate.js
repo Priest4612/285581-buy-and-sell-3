c5739551efd7b9d8bc1cb66aeb326f15890e6fb8
priest4612 <prie4612@gmail.com>
2020-06-19 14:30:43 +0300

    Исправил ошибку

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -15,7 +15,7 @@ const offerRestrict = {
 const ROOT_PATH = PROJECT_DIR;
 const FILE_NAME = path.join(ROOT_PATH, `mock.json`);
 
-const DATA_PATH = path.join(ROOT_PATH, `date`);
+const DATA_PATH = path.join(ROOT_PATH, `data`);
 const FILE_TITLES_PATH = path.join(DATA_PATH, `titles.txt`);
 const FILE_SENTENCES_PATH = path.join(DATA_PATH, `sentences.txt`);
 const FILE_CATEGORIES_PATH = path.join(DATA_PATH, `categories.txt`);
