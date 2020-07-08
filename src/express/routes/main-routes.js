f7b50ed002214bbadd2011e8c7e1820480a9e067
Priest4612 <prie4612@gmail.com>
2020-07-08 14:34:10 +0300

    Исправил замечания

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const {Router} = require(`express`);
+
+
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+mainRouter.get(`/register`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+mainRouter.get(`/login`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+mainRouter.get(`/search`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+
 
 module.exports = {
   mainRouter,
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -3,10 +3,10 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/category/:id`));
-offersRouter.get(`/add`, (req, res) => res.send(`/add`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/edit/:id`));
-offersRouter.get(`/offers/:id`, (req, res) => res.send(`/offers/:id`));
+offersRouter.get(`/category/:id`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+offersRouter.get(`/add`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+offersRouter.get(`/edit/:id`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
+offersRouter.get(`/:id`, (req, res) => res.send(`${req.baseUrl}${req.route.path}`));
 
 module.exports = {
   offersRouter,
