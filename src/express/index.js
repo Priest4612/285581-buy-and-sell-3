f491e658d1a44bf7b4fc350a37c8ffff17a0cfe5
priest4612 <prie4612@gmail.com>
2020-11-05 02:06:13 +0300

    Настроил PUG

--- src/express/index.js
+++ src/express/index.js
@@ -1,11 +1,21 @@
 'use strict';
+const path = require(`path`);
 const express = require(`express`);
 const settings = require(`../../settings`);
 const {mainRouter} = require(`./routes/main-routes`);
 
 
+// Инициализация сервера...
 const DEFAULT_PORT = settings.DEFAULT_PORT;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
+
 const app = express();
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRouter);
 
