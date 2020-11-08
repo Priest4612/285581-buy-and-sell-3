3ea8ab2cc1a436abfba4c23db7e9dee9f78fee9b
priest4612 <prie4612@gmail.com>
2020-11-08 14:44:28 +0300

    Мелкие правки

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,7 +7,7 @@ const {myRouter} = require(`./my-routes`);
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main/main`));
+mainRouter.get(`/`, (req, res) => res.render(`main/main`, {title: `Куплю Продам`}));
 mainRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`main/login`));
 mainRouter.get(`/search`, (req, res) => res.render(`main/search`));
