9d442466d2c9dfacfd2e56819c4cc214fe763fac
Priest4612 <prie4612@gmail.com>
2020-11-17 12:34:56 +0300

    Добавли логирование

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,37 +1,17 @@
 'use strict';
 
-const fs = require(`fs`);
-const pinoms = require(`pino-multi-stream`);
-
+const pino = require(`pino`);
 const {Env} = require(`../../constants`);
-const LOG_FILE_ALL = `./logs/all.log`;
-const LOG_FILE_ERRORS = `./logs/errors.log`;
 
+const LOG_FILE = `./logs/api.log`;
 const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
 const defaultLogLevel = isDevMode ? `info` : `error`;
 
-const prettyStream = pinoms.prettyStream({
-  prettyPrint: {
-    colorize: true,
-    translateTime: `dd-mm-yyyy HH:MM:ss`,
-    ignore: `hostname, pid`,
-    levelFirst: true,
-  },
-  prettifier: require(`pino-pretty`)
-});
-
-const level = process.env.LOG_LEVEL || defaultLogLevel;
-
-const streams = [
-  {level, stream: prettyStream},
-  {level, stream: fs.createWriteStream(LOG_FILE_ALL)},
-  {level: `error`, stream: fs.createWriteStream(LOG_FILE_ERRORS)}
-];
-
-const logger = pinoms({
-  name: `pino-in-express`,
-  level,
-}, pinoms.multistream(streams));
+const logger = pino({
+  name: `base-logger`,
+  level: process.env.LOG_LEVEL || defaultLogLevel,
+  prettyPrint: isDevMode
+}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
 
 module.exports = {
   logger,
